aee6e2189032ca379559129580b305f137575ba8
Carlton Joseph <carlton.joseph@gmail.com>
2019-01-12 17:05:06 -0800

    add template

--- node001_power.js
+++ node001_power.js
@@ -3,7 +3,7 @@ const { secrets } = require("./secrets.js");
 
 (async () => {
   try {
-    const browser = await puppeteer.launch({ headless: false, slowMo: 250 });
+    const browser = await puppeteer.launch({ headless: true, slowMo: 50 });
     const page = await browser.newPage();
     await page.setViewport({ width: 1280, height: 800 });
     await page.goto("http://10.0.0.200", { waitUntil: "networkidle2" });
@@ -18,6 +18,28 @@ const { secrets } = require("./secrets.js");
     await page.click('input[value="OK"]');
 
     await page.screenshot({ path: "logged-in-screen.png", fullPage: true });
+
+    /* pdf generation only works when headless is true above */
+    /*
+    await page.pdf({
+      path: "logged-in-screen.pdf",
+      format: "Letter",
+      margin: { top: "1in", bottom: "1in", left: "1in", right: "1in" }
+    });
+    */
+
+    const name = "Carlton";
+    const html = `<h1>Hello ${name}. How are you coding`;
+    const page2 = await browser.newPage();
+    await page2.setContent(html);
+    /* pdf generation only works when headless is true above */
+    await page2.pdf({
+      path: "template.pdf",
+      format: "Letter",
+      margin: { top: "1in", bottom: "1in", left: "1in", right: "1in" }
+    });
+    /*
+     */
     await browser.close();
   } catch (e) {
     console.error("node001.js", e);
